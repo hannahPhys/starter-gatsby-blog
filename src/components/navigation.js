9f99e7c72c390c57730b85ef8a180bf0bcb869fc
HAuckram <hannnah.auckram@xtra.co.nz>
2020-11-27 23:19:41 +1300

    made nav bar a bit better

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,14 +3,23 @@ import { Link } from 'gatsby'
 import styles from './navigation.module.css'
 
 export default () => (
-  <nav role="navigation" >
-    <ul className={styles.navigation}>
+  < nav role="navigation" className={styles.navigation} >
+    <ul >
       <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
+        <Link className={styles.navigationLink} to="/">Home</Link>
       </li>
       <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
+        <Link className={styles.navigationLink} to="/">Learn</Link>
+
+        <ul>
+          <li> <Link className={styles.navigationLink} to="/blog/">Weed</Link> </li>
+          <li> <Link className={styles.navigationLink} to="/blog/">Sex</Link> </li>
+          <li> <Link className={styles.navigationLink} to="/blog/">Education</Link> </li>
+
+        </ul>
       </li>
     </ul>
-  </nav>
+  </nav >
 )
+
+
