79904d00949133310405c8066b44227e7a7d0091
HAuckram <hannnah.auckram@xtra.co.nz>
2020-12-05 23:24:46 +1300

    sexy blurs

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,10 +4,13 @@ import styles from './hero.module.css'
 
 export default () => (
   <div className={styles.hero}>
-    <img
-      src={sex}
-      alt="Hero"
-    />
+    <div className={styles.heroWrapper}>
+      <img
+        className={styles.heroImage}
+        src={sex}
+        alt="Hero"
+      ></img>
+    </div>
     <div className={styles.heroDetails}>
       <h3 className={styles.heroHeadline}>Weed Wāhine</h3>
     </div>
