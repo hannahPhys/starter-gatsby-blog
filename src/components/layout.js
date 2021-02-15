4e8e269edf904756cc33356ceb37f2e2c78dcc23
HAuckram <hannnah.auckram@xtra.co.nz>
2021-02-15 23:58:26 +1300

    added basic sidebar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import './base.css'
 import Container from './container'
-import Navigation from './navigation'
+import Sidebar from './sidebar'
 
 class Template extends React.Component {
+
   render() {
     const { children } = this.props
 
     return (
       <Container>
-        <Navigation />
+        <Sidebar />
         {children}
       </Container>
     )
---
+++ src/components/sidebar.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import './sidebar.css'
+import { Link } from 'gatsby'
+
+
+//https://therichpost.com/reactjs-responsive-sidebar-template-free/
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/umd/popper.min.js'
+
+import $ from 'jquery';
+
+class Sidebar extends React.Component {
+	componentDidMount() {
+
+		// Sidebar Toggle Menu Click
+		$("#menu-toggle").on("click", function (e) {
+			$("#navwrapper").toggleClass("toggled");
+		});
+
+	}
+
+
+	render() {
+		return (
+			<div className="d-flex" id="navwrapper">
+				<div id="sidebar-wrapper">
+					<div className="sidebar-heading">Weed Wāhine </div>
+					<div className="list-group list-group-flush ">
+						<ul>
+							<li className="navigationItem">
+								<Link className="navigationLink" to="/">Home</Link>
+							</li>
+							<li className="navigationItem">
+								<Link className="navigationLink" to="/blog/">Learn</Link>
+								<ul>
+									<li> <Link className="navigationLink" to="/weed/">Weed</Link> </li>
+									<li> <Link className="navigationLink" to="/sex/">Sex</Link> </li>
+									<li> <Link className="navigationLink" to="/education/">Education</Link> </li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<nav className="navbar navbar-light">
+					<button className="navbar-toggler" id="menu-toggle" type="button" >
+						<span className="navbar-toggler-icon"></span>
+					</button>
+				</nav>
+			</div>
+		)
+	}
+}
+
+export default Sidebar
